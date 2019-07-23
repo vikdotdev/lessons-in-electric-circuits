fd1ef4886db1477e3a7f4e32f7f9743f722abe52
vikdotdev <viktorhbk@gmail.com>
2019-07-23 14:11:06 +0300

    made it working

--- index.js
+++ index.js
@@ -2,13 +2,14 @@ const path = require('path');
 const fs = require('fs');
 const shell = require('shelljs');
 
-function createDir(bookPath, format) {
-  const folderName = bookPath.replace(/html\/(\w)/, `${format}/$1`);
-  shell.mkdir('-p', folderName);
-}
 
 function stripHead(file) {
-  return shell.exec(`sed '/<head>/,/<\/head>/{//!d}' ${file}`);
+  console.log(file);
+  return shell.exec(`sed '/<head>/,/<\/head>/d' ${file}`, (code, stdout, stderr) => {
+    console.log(`Exit code: ${code}`)
+    console.log(`Out: ${stdout}`);
+    console.log(`Err: ${stderr}`);
+  });
 }
 
 function getHtmlFiles(bookPath) {
@@ -16,17 +17,20 @@ function getHtmlFiles(bookPath) {
     .readdirSync(path.resolve(bookPath))
     .filter(f => f.match('.html'))
     .map(f => `${bookPath}/${f}`)
-    .map(stripHead)
-    .sort((a, b) => a.localeCompare(b, 'en', { numeric: true }))
+    .sort((a, b) => `${a}`.localeCompare(`${b}`, 'en', { numeric: true }))
     .join(' ');
 }
 
 function makeEpub(bookPath, bookName) {
   const htmlFiles = getHtmlFiles(bookPath);
-  createDir(bookPath);
+  shell.mkdir('epub');
 
-  shell.exec(`pandoc ${path.resolve(htmlFiles)} -o ${bookName}.epub`)
+  shell.exec(`pandoc ${path.resolve(htmlFiles)} -o epub/${bookName}.epub`, (code, stdout, stderr) => {
+    console.log(`Exit code: ${code}`)
+    console.log(`Out: ${stdout}`);
+    console.log(`Err: ${stderr}`);
+  });
 }
 
-stripHead('html/DC/DC_1.html');
-//makeEpub('html/DC', 'DC - I');
+//stripHead('html/DC/DC_1.html');
+makeEpub('html/DC', 'DC-I');
