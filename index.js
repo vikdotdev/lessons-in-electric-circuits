56d193ce5af65ebb7dafa0c57d5c5db9fccb090f
vikdotdev <viktorhbk@gmail.com>
2019-07-23 15:48:17 +0300

    images are not included, unnecessary titles removed

--- index.js
+++ index.js
@@ -2,14 +2,12 @@ const path = require('path');
 const fs = require('fs');
 const shell = require('shelljs');
 
-
 function stripHead(file) {
-  console.log(file);
-  return shell.exec(`sed '/<head>/,/<\/head>/d' ${file}`, (code, stdout, stderr) => {
-    console.log(`Exit code: ${code}`)
-    console.log(`Out: ${stdout}`);
-    console.log(`Err: ${stderr}`);
-  });
+  return file.replace(/<title>.+<\/title>/gi, '');
+}
+
+function replaceImagePath(file) {
+  return file.replace(/src="(\d+).(png|jpg)"/gi, 'src="html/DC/$1.$2"');
 }
 
 function getHtmlFiles(bookPath) {
@@ -17,20 +15,35 @@ function getHtmlFiles(bookPath) {
     .readdirSync(path.resolve(bookPath))
     .filter(f => f.match('.html'))
     .map(f => `${bookPath}/${f}`)
-    .sort((a, b) => `${a}`.localeCompare(`${b}`, 'en', { numeric: true }))
-    .join(' ');
+    .sort((a, b) => `${a}`.localeCompare(`${b}`, 'en', { numeric: true }));
 }
 
-function makeEpub(bookPath, bookName) {
-  const htmlFiles = getHtmlFiles(bookPath);
-  shell.mkdir('epub');
 
-  shell.exec(`pandoc ${path.resolve(htmlFiles)} -o epub/${bookName}.epub`, (code, stdout, stderr) => {
+function makeEpub(bookName) {
+  const htmlFiles = getHtmlFiles(`html/${bookName}`);
+
+  const tempFileContents = htmlFiles
+    .map(f => fs.readFileSync(f, 'utf8'))
+    .map(replaceImagePath)
+    .map(stripHead);
+
+  shell.mkdir('-p', `temp/${bookName}`);
+  htmlFiles.forEach((filePath, i) => {
+    const tmp = filePath.split('/');
+    tmp[0] = 'temp';
+    const newPath = tmp.join('/');
+    console.log('newpath', newPath)
+    fs.writeFileSync(newPath, tempFileContents[i]);
+  });
+
+  const tempFiles = getHtmlFiles(`temp/${bookName}`);
+
+  shell.mkdir('epub');
+  shell.exec(`pandoc --metadata title="${bookName}" ${path.resolve(tempFiles.join(' '))} -o epub/${bookName}.epub`, (code, stdout, stderr) => {
     console.log(`Exit code: ${code}`)
     console.log(`Out: ${stdout}`);
     console.log(`Err: ${stderr}`);
   });
 }
 
-//stripHead('html/DC/DC_1.html');
-makeEpub('html/DC', 'DC-I');
+makeEpub('DC');
